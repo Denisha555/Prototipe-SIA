48f28bdf7872ca2b5efef833dbbe372a6b28ae9b
Denisha555 <firdiantidenisha05@gmail.com>
2025-10-11 11:15:17 +0700

    simpan perubahan

--- manager/jurnal_umum.py
+++ manager/jurnal_umum.py
@@ -7,17 +7,24 @@ class JurnalUmumPage(tk.Frame):
         super().__init__(parent)
         self.controller = controller
 
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+
         ttk.Label(self, text="Jurnal Umum", font=("Helvetica", 18, "bold")).grid(row=0, column=0, columnspan=2, pady=15)
 
-        ttk.Label(self, text="Bulan: ").grid(row=1, column=0)
-        self.combo_bulan = ttk.Combobox()
+        bulan_list = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
 
-        ttk.Label(self, text="Tahun: ").grid(row=2, column=0)
+        ttk.Label(self, text="Bulan: ").grid(row=1, column=0, sticky="e", pady=5)
+        self.combo_bulan = ttk.Combobox(self, width=27, state="readonly")
+        self.combo_bulan.grid(row=1, column=1, sticky="w", pady=5)
+        self.combo_bulan['values'] = bulan_list
+
+        ttk.Label(self, text="Tahun: ").grid(row=2, column=0, sticky="e", pady=5)
         self.entry_tahun = ttk.Entry(self, width=30)
-        self.entry_tahun.grid(row=2, column=1)
+        self.entry_tahun.grid(row=2, column=1, sticky="w", pady=5)
 
         self.tree = ttk.Treeview(self, columns=("tanggal", "keterangan", "debit", "kredit"), show="headings")
-        self.tree.grid(row=3, column=0, columnspan=2)
+        self.tree.grid(row=3, column=0, columnspan=2, pady=5)
 
         self.tree.heading("tanggal", text="Tanggal")
         self.tree.heading("keterangan", text="Keterangan")
@@ -32,8 +39,36 @@ class JurnalUmumPage(tk.Frame):
 
         conn = sqlite3.connect("data_keuangan.db")
         c = conn.cursor()
-        c.execute("SELECT ")
+        c.execute("""
+            SELECT transaksi_penjualan_id, kategori, tanggal, total
+            FROM transaksi_penjualan
+            WHERE strftime('%m', tanggal) = ? AND strftime('%Y', tanggal) = ?
+        """, (bulan, tahun))
 
+        kas_masuk = c.fetchall()
+
+        c.execute("""
+            SELECT transaksi_pembelian_id, kategori, tanggal, total
+            FROM transaksi_pembelian
+            WHERE strftime('%m', tanggal) = ? AND strftime('%Y', tanggal) = ?
+        """), (bulan, tahun)
+
+        kas_keluar = c.fetchall()
+
+        conn.close()
+
+        
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    class DummyController:
+        def show_frame(self, page_name):
+            pass
+
+    app = JurnalUmumPage(root, DummyController)
+    app.pack(fill="both", expand=True)
+    root.mainloop()
     
 
 
